5b5f4e83dabfdedb62a20b894d769cba7f4fb750
marcelo <marcelohenrique.backend@gmail.com>
2022-09-22 19:15:07 -0300

    Percorrendo array

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-Array-001.js
@@ -0,0 +1,11 @@
+// Iremos utilizar esse array para realizar os próximos exercícios:
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let number of numbers) {
+  console.log(number);
+}
