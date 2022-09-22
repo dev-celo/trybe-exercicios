5f5a2c76a2ffe3d95c22c35afc64520301f92255
marcelo <marcelohenrique.backend@gmail.com>
2022-09-22 19:16:43 -0300

    Somando array

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-Array-002.js
@@ -0,0 +1,14 @@
+// Iremos utilizar esse array para realizar os próximos exercícios:
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Some todos os valores contidos no array e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0
+
+for(let number of numbers) {
+  sum += number;
+}
+
+console.log(sum);
