e7e4c28f0b5562a5ad0f1771bd746e29cfb0adc2
marcelo <marcelohenrique.backend@gmail.com>
2022-09-22 18:42:10 -0300

    Maior número do array

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Array.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Array.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
-let media = 0;
+let larger = 0;
 
-for(let number of numbers) {
-  sum += number;
-  media = sum/numbers.length
+for (let index = 0; index < numbers.length; index++) {
+  if(numbers[index] > larger){
+    larger = numbers[index]
+  }
 }
 
-media > 20 ? console.log('Maior que 20') : console.log('Menor que 20');
+console.log(`Maior número: ${larger}`)
