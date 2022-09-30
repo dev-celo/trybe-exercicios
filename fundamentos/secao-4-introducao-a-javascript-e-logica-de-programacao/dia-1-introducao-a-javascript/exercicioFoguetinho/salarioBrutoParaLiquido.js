fb481656ba8128e986620b5d10fbd32e80573910
marcelo <marcelohenrique.backend@gmail.com>
2022-09-30 12:37:13 -0300

    Removendo comentários

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-a-javascript/exercicioFoguetinho/salarioBrutoParaLiquido.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-a-javascript/exercicioFoguetinho/salarioBrutoParaLiquido.js
@@ -1,14 +1,3 @@
-// Até R$ 1.903,98: isento de imposto de renda
-
-// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
-
-// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
-
-// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
-
-// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
-
-
 let salarioBruto = 3000;
 
 function descontoInss(salarioBruto) {
