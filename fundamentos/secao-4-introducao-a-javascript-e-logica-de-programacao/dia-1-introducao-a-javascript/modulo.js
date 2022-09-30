dd85793fd91a90af1a4348c8987e97fe524895e7
marcelo <marcelohenrique.backend@gmail.com>
2022-09-30 09:13:18 -0300

    Resolucao exercicio resto da divisão

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-a-javascript/modulo.js
@@ -0,0 +1,11 @@
+//Adição entre dois números
+
+let numberA = 2;
+let numberB = 2;
+
+function divisionModule(numA, numB) {
+  // console.log(numA % numB);
+  return (numA % numB);
+}
+
+divisionModule(numberA, numberB);
