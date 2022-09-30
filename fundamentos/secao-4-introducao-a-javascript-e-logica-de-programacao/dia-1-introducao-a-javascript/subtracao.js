c5eb3572f44b09d7dbdee522f7ff89676412baff
marcelo <marcelohenrique.backend@gmail.com>
2022-09-30 09:07:52 -0300

    Resolucao exercicio subtração

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-a-javascript/subtracao.js
@@ -0,0 +1,11 @@
+//Adição entre dois números
+
+let numberA = 13;
+let numberB = 13;
+
+function subtraction(numA, numB) {
+  // console.log(numA - numB);
+  return (numA - numB);
+}
+
+subtraction(numberA, numberB);
