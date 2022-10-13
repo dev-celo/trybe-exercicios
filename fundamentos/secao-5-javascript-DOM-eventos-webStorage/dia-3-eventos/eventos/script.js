015681037b7fd2cdb54ce909797ef34691c4e9f3
marcelo <marcelohenrique.backend@gmail.com>
2022-10-13 11:08:52 -0300

    Resolução do requisito 1

--- fundamentos/secao-5-javascript-DOM-eventos-webStorage/dia-3-eventos/eventos/script.js
+++ fundamentos/secao-5-javascript-DOM-eventos-webStorage/dia-3-eventos/eventos/script.js
@@ -14,3 +14,31 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const daysOfCalendar = () => {
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const days = document.querySelector('#days');
+
+  for (let index = 0; index < decemberDaysList.length; index++) {
+    const element = decemberDaysList[index];
+    
+    const li = document.createElement('li');
+    li.classList = 'day';
+    li.innerText = element;
+
+    if (element === 24 || element === 25 || element === 31) li.classList.add('holiday');
+    
+    if (element === 4 || element === 11 || element === 18 || element === 25) li.classList.add('friday');
+
+    days.appendChild(li)
+  }
+
+
+};
+
+
+
+
+
+
+daysOfCalendar();
