1ca5afc652db68e7be3f3055006863364326de8a
marcelo <marcelohenrique.backend@gmail.com>
2022-10-17 20:15:14 -0300

    Criando função que ordena array!

---
+++ fundamentos/secao-7-javascript-es6-e-testes-unitarios/dia-1-let-const-arrowFunctions-e-templateLiterals/parte-II/exercicio-2.js
@@ -0,0 +1,18 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = array => {
+// Seu código aqui.
+  let menor = 10000;
+  for(let index = 0; index < array.length; index += 1){
+    for(let indexCompara = 0; indexCompara < array.length; indexCompara += 1){
+      if(array[indexCompara] > array[index]) {
+        let aux = array[index];
+        array[index] = array[indexCompara];
+        array[indexCompara] = aux;
+      }
+    }
+  }
+  return array;
+}
+
+console.log(sortOddsAndEvens(oddsAndEvens));
