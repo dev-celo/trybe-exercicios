5f56166d4aa64c4ba7025a9df96b41e2b6f9acea
marcelo <marcelohenrique.backend@gmail.com>
2022-10-25 10:59:05 -0300

    Resolvendo e testanto exercício parte I

--- fundamentos/secao-8-higher-order-functions-javascript-es6/parte-I/new-person-hired.test.js
+++ fundamentos/secao-8-higher-order-functions-javascript-es6/parte-I/new-person-hired.test.js
@@ -1,7 +1,23 @@
 const newPersonHired = require('./new-person-hired');
 
-describe('Testa função new-person-hired', () => {
-  it('Testa se a função retorna true', () => {
-    expect(newPersonHired()). toBe(true)
+describe('Testa função person', () => {
+  it('Testa se a função person retorna um objeto', () => {
+    expect(typeof newPersonHired.person()).toBe('object');
+  })
+  it('Testa se a função person retorna um objeto com as chaves nome e email caso preenchida', () => {
+    expect(newPersonHired.person('Marcelo Henrique')). toEqual({ nome: 'Marcelo Henrique', email: ' marcelo_henrique@gmail.com' });
+  })
+});
+
+describe('Testa função newEmployees', () => {
+  it('Testa se a função newEmployees retorna um objeto', () => {
+    expect(typeof newPersonHired.newEmployees(newPersonHired.person)).toBe('object');
+  })
+  it('Testa se a função newEmployees retorna um objeto com as chaves id, nome e email caso preenchida', () => {
+    expect(newPersonHired.newEmployees(newPersonHired.person)).toEqual({
+      id1: { nome: 'Pedro Guerra', email: ' pedro_guerra@gmail.com' },
+      id2: { nome: 'Luiza Drumond', email: ' luiza_drumond@gmail.com' },
+      id3: { nome: 'Carla Paiva', email: ' carla_paiva@gmail.com' }
+    });
   })
 });
