cd08ab00fe17b1fa0e7b380f62814ad1358b1953
marcelo <marcelohenrique.backend@gmail.com>
2022-10-25 12:35:06 -0300

    Resolvendo e testanto exercício parte III

--- fundamentos/secao-8-higher-order-functions-javascript-es6/parte-III/exam-auto-corrector.js
+++ fundamentos/secao-8-higher-order-functions-javascript-es6/parte-III/exam-auto-corrector.js
@@ -1,3 +1,21 @@
-const corrector = () => true;
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-module.exports = corrector;
+const examCorrector = (arrayCorrectGrades, arrayGradesOfStudent, evaluator) => evaluator(arrayCorrectGrades, arrayGradesOfStudent);
+
+const autoCorrector = (RIGHT_ANSWERS, STUDENT_ANSWERS) => {
+  let totalPontos = 0;
+
+  for(let index = 0; index < RIGHT_ANSWERS.length; index += 1) {
+    if (RIGHT_ANSWERS[index] === STUDENT_ANSWERS[index]) {
+      totalPontos += 1;
+    } else if (RIGHT_ANSWERS[index] !== STUDENT_ANSWERS[index] && STUDENT_ANSWERS[index] !== 'N.A') {
+      totalPontos -= 0.5;
+    }
+  }
+  return totalPontos;
+};
+
+console.log(examCorrector(RIGHT_ANSWERS, STUDENT_ANSWERS, autoCorrector));
+
+module.exports = {examCorrector, autoCorrector};
--- fundamentos/secao-8-higher-order-functions-javascript-es6/parte-III/exam-auto-corrector.test.js
+++ fundamentos/secao-8-higher-order-functions-javascript-es6/parte-III/exam-auto-corrector.test.js
@@ -1,7 +1,29 @@
 const examAutoCorrector = require('./exam-auto-corrector');
+const array1 = ['a', 'b'];
 
-describe('Testa função exam-auto-corrector', () => {
-  it('Testa se a função retorna true', () => {
-    expect(examAutoCorrector()). toBe(true)
+describe('Testa função examCorrector', () => {
+  it('Testa se a função examCorrector retorna um number', () => {
+    expect(typeof examAutoCorrector.examCorrector(array1, array1, examAutoCorrector.autoCorrector)). toBe('number')
+  })
+  it('Testa se a função examCorrector é uma função', () => {
+    expect(typeof examAutoCorrector.examCorrector). toBe('function')
+  })
+});
+
+describe('Testa função autoCorrector', () => {
+  it('Testa se a função autoCorrector retorna um number', () => {
+    expect(typeof examAutoCorrector.autoCorrector(['a'], ['a'],)).toBe('number')
+  })
+  it('Testa se a função autoCorrector é uma função', () => {
+    expect(typeof examAutoCorrector.autoCorrector).toBe('function')
+  })
+  it('Testa se a função autoCorrector ao receber dois arrays[2] iguais retorna 2', () => {
+    expect(examAutoCorrector.examCorrector(array1, array1, examAutoCorrector.autoCorrector)).toBe(2)
+  })
+  it('Testa se a função autoCorrector ao receber dois arrays[2] diferente retorna 0.5', () => {
+    expect(examAutoCorrector.examCorrector(array1, ['a', 'a'], examAutoCorrector.autoCorrector)).toBe(0.5)
+  })
+  it('Testa se a função autoCorrector ao receber dois arrays[2] diferente um com valor "N.A" retorna 1', () => {
+    expect(examAutoCorrector.examCorrector(array1, ['a', 'N.A'], examAutoCorrector.autoCorrector)).toBe(1)
   })
 });
