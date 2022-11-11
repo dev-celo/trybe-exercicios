7af1044b7ef3305903c037c1ea03646a7a0d382d
marcelo <marcelohenrique.backend@gmail.com>
2022-11-11 11:05:06 -0300

    Linter aprovado!

--- front-end/secao-1-frontend-e-javascript-assicrono/README.md
+++ front-end/secao-1-frontend-e-javascript-assicrono/README.md
@@ -9,4 +9,4 @@
 - [x] O formulário deve possuir: campo para inserir o texto a ser validado, um dropdown para selecionar qual a validação deve ser feita, um botão para executar a validação e um texto que informe se o valor é válido ou não
 - [x] Faça, pelo menos, cinco validações utilizando a biblioteca validator. Aproveite e confira a documentação para ver exatamente como utilizá-la!
 - [x] Rode sua aplicação utilizando o vite
-- [ ] Teste se sua aplicação é aprovada pelo lint da Trybe
+- [x] Teste se sua aplicação é aprovada pelo lint da Trybe
--- front-end/secao-1-frontend-e-javascript-assicrono/src/main.js
+++ front-end/secao-1-frontend-e-javascript-assicrono/src/main.js
@@ -4,6 +4,7 @@ const inputValue = document.getElementById('getValue');
 const seletor = document.getElementById('option');
 const button = document.getElementById('button');
 const resposta = document.getElementById('answer');
+const uuid4 = 4;
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
@@ -12,9 +13,9 @@ button.addEventListener('click', (event) => {
     cpf: validator.isTaxID(inputValue.value, 'pt-BR'),
     hexColor: validator.isHexadecimal(inputValue.value),
     email: validator.isEmail(inputValue.value),
-    uuid: validator.isUUID(inputValue.value, 4),
+    uuid: validator.isUUID(inputValue.value, uuid4),
     url: validator.isURL(inputValue.value),
-  }
+  };
 
-  resposta.innerHTML = `O campo validado é ${validacao[seletor.value]}`
+  resposta.innerHTML = `O campo validado é ${validacao[seletor.value]}`;
 });
