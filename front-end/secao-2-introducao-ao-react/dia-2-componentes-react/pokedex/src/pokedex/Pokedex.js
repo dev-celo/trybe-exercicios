421181bf1ebb435229ab72174e73b8105215e403
marcelo <marcelohenrique.backend@gmail.com>
2022-11-23 20:48:32 -0300

    Requisito 1 ok - Pokedex component

---
+++ front-end/secao-2-introducao-ao-react/dia-2-componentes-react/pokedex/src/pokedex/Pokedex.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Pokedex extends React.Component { 
+  render() { 
+    return (
+      <header>
+        <h1>Pokédex</h1>
+        <ul></ul>
+      </header>
+    );
+  }
+}
+ 
+export default Pokedex;
