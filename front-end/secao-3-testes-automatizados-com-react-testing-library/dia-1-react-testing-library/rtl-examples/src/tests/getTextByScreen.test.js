e49e282fb2afaae253176a9ed7aca6528a5dedc7
marcelo <marcelohenrique.backend@gmail.com>
2023-01-19 09:05:41 -0300

    Testando clique no botão enviar do formulário.

--- front-end/secao-3-testes-automatizados-com-react-testing-library/dia-1-react-testing-library/rtl-examples/src/tests/getTextByScreen.test.js
+++ front-end/secao-3-testes-automatizados-com-react-testing-library/dia-1-react-testing-library/rtl-examples/src/tests/getTextByScreen.test.js
@@ -2,16 +2,40 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
-test('Verificando se existe o campo Email.', () => {
+beforeEach(() => {
   render(<App />);
-  const inputEmail = screen.getByLabelText('Email');
-  expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail).toHaveProperty('type', 'email');
-});
+}) 
 
-test('Verificando se existe um botão', () => {
-  render(<App />);
-  const btn = screen.getByRole('button');
-  expect(btn).toBeInTheDocument();
-});
+describe('Testando se ao clicar na tela o nome é impresso na mesma', () => {
+  it('Verificando se existe o campo Email.', () => {
+    const inputEmail = screen.getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail).toHaveProperty('type', 'email');
+  });
+  
+  it('Verificando se existem dois botão', () => {
+    const btn = screen.getAllByRole('button');
+    expect(btn).toHaveLength(2);
+  });
+  
+  it('Verificando se o botão e o campo email estão funcionando.', () => {
+    const EMAIL_USER = 'email@email.com';
+
+    const inputEmail = screen.getByTestId('email');
+    const buttonSend = screen.getByTestId('id-send');
+    expect(inputEmail).toBeInTheDocument();
+    expect(buttonSend).toBeInTheDocument();
+
+    const textEmail = screen.getByTestId('id-email-user');
+    expect(textEmail).toBeInTheDocument();
+    expect(textEmail).toHaveTextContent('Valor:')
+    
+    userEvent.type(inputEmail, 'marcelo@teste.com');
+    userEvent.click(buttonSend);
+
+    expect(textEmail).toHaveTextContent('Valor: marcelo@teste.com');
+  });
+});  
+
