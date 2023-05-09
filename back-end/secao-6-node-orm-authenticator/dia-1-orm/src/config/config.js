23378e1687f11bccbee5dc1852f23e01fe51c130
marcelo <marcelohenrique.backend@gmail.com>
2023-05-09 17:30:45 -0300

    Iniciando projeto migration, model e service de Books estruturado

---
+++ back-end/secao-6-node-orm-authenticator/dia-1-orm/src/config/config.js
@@ -0,0 +1,14 @@
+// src/config/config.js
+const config = {
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DATABASE || 'orm_exercise_day_one',
+  host: process.env.MYSQL_HOST || 'localhost',
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
